8a010563d8aa6690f32458cee3eee2d0532d75bc
Venkata Ramanarao Ponnapalli <Venkata_Ramanarao_Ponnapalli@gap.com>
2019-02-19 19:44:36 -0800

    This update included to produce topic thru producer

--- src/main/java/com/kafka/producer/kafkaproducer/service/KafkaProducer001.java
+++ src/main/java/com/kafka/producer/kafkaproducer/service/KafkaProducer001.java
@@ -1,47 +1,46 @@
-//package com.kafka.producer.kafkaproducer.service;
-//
-//import org.apache.kafka.clients.producer.KafkaProducer;
-//import org.apache.kafka.clients.producer.ProducerRecord;
-//import org.apache.kafka.clients.producer.RecordMetadata;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.kafka.support.SendResult;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class KafkaProducer001 {
-//
-//
-//	@Autowired
-//	private KafkaProducer<String, String> kafkaProducer;
-//    
-//	@Autowired
-//	private String[] topic;
-//   
-//    @Autowired
-//    public KafkaProducer001(KafkaProducer<String, String> kafkaProducer, String[] topic) {
-//		this.kafkaProducer = kafkaProducer;
-//		this.topic = topic;
-//	}
-//
-//    public RecordMetadata sendMessage(String eventFeed) {
-//        RecordMetadata producerResponse = null;
-//      
-//       for( int i=0; i<topic.length; i++) {
-//        
-//        try {
-//			ProducerRecord<String, String> record = new ProducerRecord<>(topic[i], eventFeed);
-//            producerResponse = kafkaProducer.send(record).get();
-//            System.out.println("Message send to topic::" + record.topic()+ "Partition is"+ record.partition() + 
-//            		"Timne is" + record.timestamp());
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+package com.kafka.producer.kafkaproducer.service;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducer001 {
+	
+	@Autowired
+	private Producer<String, String> kafkaProducer;
+    
+	@Autowired
+	private String topicc;
+   
+	
+	@Autowired
+    public KafkaProducer001(Producer<String, String> kafkaProducer, String topicc) {
+		this.kafkaProducer = kafkaProducer;
+		this.topicc = topicc;
+	}
+
+    
+    public RecordMetadata sendMessage(String eventFeed) {
+        RecordMetadata producerResponse = null;
+      
+//       for( int i=0; i<topicc.length; i++) {
+        
+        try {
+			ProducerRecord<String, String> record = new ProducerRecord<>(topicc, eventFeed);
+            producerResponse = kafkaProducer.send(record).get();
+            System.out.println("Message send to topic::" + record.topic()+ "Partition is"+ record.partition() + 
+            		"Time is" + record.timestamp());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 //       }
-//        return producerResponse;
-//        
-//    }
-//   
-//    
-//}
+        return producerResponse;
+        
+    }
+   
+    
+}
